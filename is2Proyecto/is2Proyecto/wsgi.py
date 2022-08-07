bcf12bdd3fdae50961c4fec9228edd0cd8d0716b
Victor Cubas <vhcubas91@gmail.com>
2022-08-07 10:06:13 -0400

    se creo un proyecto django

---
+++ is2Proyecto/is2Proyecto/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for is2Proyecto project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/4.0/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'is2Proyecto.settings')
+
+application = get_wsgi_application()
