09c30c0671ed2cf3bd2ce75ba6fab03ad250303e
Victor Cubas <escurracaceres.andy@fpuna.edu.py>
2022-08-09 13:22:22 -0400

    agregamos el metodo iniciarSesion() a la vista

--- is2Proyecto/gestionProyectoApp/views.py
+++ is2Proyecto/gestionProyectoApp/views.py
@@ -1,3 +1,4 @@
+from ast import Return
 from os import curdir
 from django.shortcuts import redirect, render
 from django.http import HttpResponse
@@ -5,3 +6,8 @@ from django.http import HttpResponse
 
 def login(request):
 	return render(request, 'index.html')
+
+def iniciarSesion(request):
+    email = request.POST['yourEmail']
+    contrasenia = request.POST['yourPassword']
+    return render(request, 'homeProyecto.html', {'email': email})
