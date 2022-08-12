5af477429b3074f4d993efcb7f67cdddee2dabdf
natydominguez <110267297+natydominguez@users.noreply.github.com>
2022-08-12 19:13:14 -0400

    Update settings.py
    Se corrige language_code y time_zone

--- is2Proyecto/is2Proyecto/settings.py
+++ is2Proyecto/is2Proyecto/settings.py
@@ -104,9 +104,9 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/4.0/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'es-py'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'America/Asuncion'
 
 USE_I18N = True
 
