afd70127cb592367d7e6dd5f578db4a48b39133b
Victor Cubas <vhcubas91@gmail.com>
2022-08-18 17:06:35 -0400

    actualizamos el setting para poder manejar postgres

--- is2Proyecto/is2Proyecto/settings.py
+++ is2Proyecto/is2Proyecto/settings.py
@@ -76,8 +76,12 @@ WSGI_APPLICATION = 'is2Proyecto.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'GestionProyectos',
+        'USER': 'postgres',
+        'PASSWORD': 'unfor007',
+        'HOST': '127.0.0.1',
+        'DATABASE_PORT': '5432',
     }
 }
 
