ac92cc07928a5d8f8e9efa6ff711fa7e148b4011
Victor Cubas <vhcubas91@gmail.com>
2022-08-18 17:07:55 -0400

    agregamos Proyecto y Usuario al model para probar la BD

--- is2Proyecto/gestionProyectoApp/models.py
+++ is2Proyecto/gestionProyectoApp/models.py
@@ -1,3 +1,21 @@
+import email
 from django.db import models
 
 # Create your models here.
+class Proyecto(models.Model):
+    nombre = models.CharField(max_length=50)
+    idProyecto = models.CharField(primary_key=True, max_length=6)
+   
+    def __str__(self):
+        texto = '{0} ({1})'
+        return texto.format(self.nombre, self.codigo)
+    
+class Usuario(models.Model):
+    nombre = models.CharField(max_length=50)
+    email = models.EmailField()
+    idUsuario = models.CharField(primary_key=True, max_length=6)
+   
+    def __str__(self):
+        texto = '{0} ({1}) {2}'
+        return texto.format(self.nombre, self.email, self.idUsuario)
+    
