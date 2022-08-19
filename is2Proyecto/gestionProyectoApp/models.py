d0502e1fd31bacda74a93f2e75d7b4f8227b37c4
Victor Cubas <vhcubas91@gmail.com>
2022-08-19 13:55:54 -0400

    agregamos las demas relaciones MM entre usuario y proyectos, usuario y permisos

--- is2Proyecto/gestionProyectoApp/models.py
+++ is2Proyecto/gestionProyectoApp/models.py
@@ -5,6 +5,7 @@ from django.db import models
 class Proyecto(models.Model):
     nombre = models.CharField(max_length=50)
     idProyecto = models.CharField(primary_key=True, max_length=6)
+    
    
     def __str__(self):
         texto = '{0} ({1})'
@@ -15,38 +16,26 @@ class Proyecto(models.Model):
         verbose_name = 'Proyecto'
         db_table = 'Proyecto'
     
-class Usuario(models.Model):
-    nombre = models.CharField(max_length=50)
-    email = models.EmailField()
-    idUsuario = models.CharField(primary_key=True, max_length=6)
-   
-    def __str__(self):
-        texto = '{0} ({1}) {2}'
-        return texto.format(self.nombre, self.email, self.idUsuario)
-    
-    #personalizamos la tabla en posgres
-    class Meta:
-        verbose_name = 'Usuario'
-        db_table = 'Usuario'
 
 class Formulario(models.Model):
     nombre = models.CharField(max_length=50)
     tipo = models.CharField(max_length=4)
+    descripcion = models.CharField(max_length=200, default='')
     idFormulario = models.CharField(primary_key=True, max_length=6)
-    #idFormulario = models.ForeignKey(Formulario, null=True, on_delete=models.CASCADE)
    
    #personalizamos la tabla en posgres
     class Meta:
         verbose_name = 'Formulario'
         db_table = 'Formulario'
-    #def __str__(self):
-    #    texto = '{0} ({1}) {2}'
-    #    return texto.format(self.nombre, self.email, self.idUsuario)
+    def __str__(self):
+        texto = '{} {} {} {}'
+        return texto.format(self.nombre, self.tipo,
+                            self.descripcion, self.idFormulario)
     
 
 class Permiso(models.Model):
     nombre = models.CharField(max_length=50)
-    descripcion = models.CharField(max_length=200)
+    descripcion = models.CharField(max_length=200, default='')
     idPermiso = models.CharField(primary_key=True, max_length=6)
     idFormulario = models.ForeignKey(Formulario, null=True, on_delete=models.CASCADE)
     
@@ -64,7 +53,7 @@ class Rol(models.Model):
     nombre = models.CharField(max_length=50)
     descripcion = models.CharField(max_length=200)
     idRol = models.CharField(primary_key=True, max_length=6)
-    formulario = models.ManyToManyField(Permiso, related_name='rol_permiso')
+    permisos = models.ManyToManyField(Permiso, related_name='rol_permiso')
     
     #personalizamos la tabla en posgres
     class Meta:
@@ -74,3 +63,19 @@ class Rol(models.Model):
     #def __str__(self):
     #    texto = '{0} ({1}) {2}'
     #    return texto.format(self.nombre, self.email, self.idUsuario)
+    
+class Usuario(models.Model):
+    nombre = models.CharField(max_length=50)
+    email = models.EmailField()
+    idUsuario = models.CharField(primary_key=True, max_length=6)
+    roles = models.ManyToManyField(Rol, related_name='usuario_rol')
+    proyectos = models.ManyToManyField(Proyecto, related_name='usuario_proyecto')
+   
+    def __str__(self):
+        texto = '{0} ({1}) {2}'
+        return texto.format(self.nombre, self.email, self.idUsuario)
+    
+    #personalizamos la tabla en posgres
+    class Meta:
+        verbose_name = 'Usuario'
+        db_table = 'Usuario'
