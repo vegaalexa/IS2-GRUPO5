31df848e9f46f67784a9339cd08c9232298f516f
Victor Cubas <vhcubas91@gmail.com>
2022-08-22 16:26:03 -0400

    agregamos la vista "usuario", "registrarUsuario" y "eliminarUsuario""

--- is2Proyecto/gestionProyectoApp/views.py
+++ is2Proyecto/gestionProyectoApp/views.py
@@ -1,8 +1,9 @@
 from ast import Return
+import email
 from os import curdir
 from django.shortcuts import redirect, render
 from django.http import HttpResponse
-
+from .models import Usuario
 
 def login(request):
 	return render(request, 'index.html')
@@ -12,5 +13,32 @@ def iniciarSesion(request):
     contrasenia = request.POST['yourPassword']
     return render(request, 'homeProyecto.html', {'email': email})
 
-def seguridad(request):
-    return render(request, 'seguridad.html')
+def seguridad(request, email):
+    return render(request, 'seguridad.html', {'email': email})
+
+def usuario(request, email):
+    listaUsuarios = Usuario.objects.all()
+    return render(request, 'usuario.html', {'usuarios': listaUsuarios,
+                                            'email':email})
+
+def registrarUsuario(request, email):
+    print('llega aca??' + email)
+    print('llega o no?')
+    #codigo = request.POST.get('txtCodigo')
+    nombre = request.POST.get('txtNombre')
+    email1 = request.POST.get('txtEmail')
+    
+    usuario = Usuario.objects.create(nombre=nombre, email=email1)
+    listaUsuarios = Usuario.objects.all()
+    return render(request, 'usuario.html', {'usuarios': listaUsuarios,
+                                            'email':email})
+    #return render(request, 'usuario.html', {'email':email})
+    #return redirect('/usuario/<email>', email)
+
+def eliminarUsuario(request, email, emailAEliminar):
+    usuario = Usuario.objects.get(email=emailAEliminar)
+    usuario.delete()
+    
+    listaUsuarios = Usuario.objects.all()
+    return render(request, 'usuario.html', {'usuarios': listaUsuarios,
+                                            'email':email})
