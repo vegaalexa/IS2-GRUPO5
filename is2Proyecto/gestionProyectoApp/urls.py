a25cdf7c1443ad4b684e2256312210d27549b5ca
natydominguez <110267297+natydominguez@users.noreply.github.com>
2022-08-26 18:44:11 -0400

    Se agrega path para carga de Rol

--- is2Proyecto/gestionProyectoApp/urls.py
+++ is2Proyecto/gestionProyectoApp/urls.py
@@ -8,12 +8,18 @@ urlpatterns = [
  	path('usuario/<emailAdmin>', views.usuario),
   	path('registrarUsuario/<emailAdmin>', views.registrarUsuario),
    	path('eliminarUsuario/<emailAdmin>/<emailAEliminar>', views.eliminarUsuario),
-    path('edicionUsuario/<emailAdmin>/<emailAEditar>', views.edicionUsuario),
-    path('editarUsuario/<emailAdmin>/<emailAEditar>', views.editarUsuario),
-    #PERMISOS
-    path('permiso/<emailAdmin>', views.permiso),
-    path('registrarPermiso/<emailAdmin>', views.registrarPermiso),
-    path('eliminarPermiso/<emailAdmin>/<idPermisoAEliminar>', views.eliminarPermiso),
-    path('edicionPermiso/<emailAdmin>/<idPermisoAEditar>', views.edicionPermiso),
-    path('editarPermiso/<emailAdmin>/<idPermisoAEditar>', views.editarPermiso)
+	path('edicionUsuario/<emailAdmin>/<emailAEditar>', views.edicionUsuario),
+	path('editarUsuario/<emailAdmin>/<emailAEditar>', views.editarUsuario),
+	#PERMISOS
+	path('permiso/<emailAdmin>', views.permiso),
+	path('registrarPermiso/<emailAdmin>', views.registrarPermiso),
+	path('eliminarPermiso/<emailAdmin>/<idPermisoAEliminar>', views.eliminarPermiso),
+	path('edicionPermiso/<emailAdmin>/<idPermisoAEditar>', views.edicionPermiso),
+	path('editarPermiso/<emailAdmin>/<idPermisoAEditar>', views.editarPermiso),
+	#ROL
+	path('rol/<emailAdmin>', views.rol),
+	path('registrarRol/<emailAdmin>', views.registrarRol),
+	path('eliminarRol/<emailAdmin>/<idRolAEliminar>', views.eliminarRol),
+	path('edicionRol/<emailAdmin>/<idRolAEditar>', views.edicionRol),
+	path('editarRol/<emailAdmin>/<idRolAEditar>', views.editarRol)
 ]
