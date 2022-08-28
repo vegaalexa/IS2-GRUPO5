1578e4baa2db89d39c7c7c2cb26087a3d6b7d1d8
Victor Cubas <vhcubas91@gmail.com>
2022-08-28 19:45:05 -0400

    Eliminacion de usuario: Se agrega un archivo de javascript para una eliminacion mas segura

---
+++ is2Proyecto/gestionProyectoApp/static/js/gestionUsuario.js
@@ -0,0 +1,17 @@
+
+
+
+(function () {
+
+    const btnEliminacion = document.querySelectorAll(".btnEliminacion");
+
+    btnEliminacion.forEach(btn => {
+        btn.addEventListener('click', (e) => {
+            const confirmacion = confirm('¿Seguro de eliminar el usuario?');
+            if (!confirmacion) {
+                e.preventDefault();
+            }
+        });
+    });
+    
+})();
