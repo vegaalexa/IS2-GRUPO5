8b8068496338bbc7fcf501da6383b342e4ca26d5
Victor Cubas <vhcubas91@gmail.com>
2022-09-10 13:59:07 -0400

    Moficicamos el msj al eliminar (usuario, proyecto, backlog, roles, etc)

--- is2Proyecto/gestionProyectoApp/static/js/gestionPermisosCrud.js
+++ is2Proyecto/gestionProyectoApp/static/js/gestionPermisosCrud.js
@@ -5,7 +5,7 @@
 
     btnEliminacion.forEach(btn => {
         btn.addEventListener('click', (e) => {
-            const confirmacion = confirm('¿Seguro de eliminar el usuario?');
+            const confirmacion = confirm('¿Seguro que quieres eliminar?');
             if (!confirmacion) {
                 e.preventDefault();
             }
