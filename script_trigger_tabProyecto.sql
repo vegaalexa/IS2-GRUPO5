5fd7d3ec1d172dc050f41592c8caec39887104b3
natydominguez <110267297+natydominguez@users.noreply.github.com>
2022-10-25 23:50:35 -0300

    Se agrega comentarios

--- script_trigger_tabProyecto.sql
+++ script_trigger_tabProyecto.sql
@@ -6,13 +6,13 @@ declare
 	nv_cant_finalizado integer:=0;
 	cur_proyectos CURSOR FOR SELECT * FROM "Proyecto" where "estado"<>'F'; 
 begin
-	FOR reg IN cur_proyectos LOOP
+	for reg IN cur_proyectos loop
 		--Cantidad de SprintBacklogs por proyecto
 		select count(*) 
 		into nv_cant_sprint
 		from "Proyecto" p
 		join "BackLog" b on p."idProyecto"= b."proyecto_id"
-		left join "SprintBackLog" sb on b."idBackLog"=sb."backLog_id"
+		join "SprintBackLog" sb on b."idBackLog"=sb."backLog_id"
 		where p."estado"<>'F' and p."idProyecto"=reg."idProyecto";
 
 		--Cantidad de SB finalizados
@@ -22,13 +22,13 @@ begin
 		join "BackLog" b on p."idProyecto"= b."proyecto_id"
 		join "SprintBackLog" sb on b."idBackLog"=sb."backLog_id"
 		where sb."estado"='F' and p."idProyecto"=reg."idProyecto";
-		
+		--Si todos los sb del proyecto tienen estado F (finalizado) se ejecuta el update correspondiente
 		if (nv_cant_sprint<>0 and nv_cant_finalizado<>0) and nv_cant_sprint=nv_cant_finalizado then
 			UPDATE public."Proyecto" SET "estado"='F' where "idProyecto"=reg."idProyecto";
 		end if;
 	end loop;
 	return new;
-End
+end
 $$
 Language plpgsql;
 /
